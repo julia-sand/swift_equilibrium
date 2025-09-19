fd6dd69df99d101f33a132240b4cfca022b1f4a4
Julia Sanders <73934921+julia-sand@users.noreply.github.com>
2025-09-19 10:45:31 +0300

    organise batch scripts to match paper fig numbers

---
+++ src/scripts/fig1.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=00:15:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+
+module load julia
+julia --project=. -- ../src/direct/ipopt_equil.jl 3 0.01 "harmonic" 1.4 "none"&
+julia --project=. -- ../src/indirect/diffeq_equil.jl 3 0.01 "harmonic" 1.4 "none"&
+julia --project=. -- ../src/direct/slowfast.jl 3 0.01 "harmonic" 1.4 "none"&
+julia --project=. -- ../src/indirect/slowfast.jl 3 0.001 "harmonic" 1.4 "none"&
+wait
